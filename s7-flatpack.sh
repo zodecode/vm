338dec19becbaf5daca30aed57f3017c8c85112b
zodecode <132285806+zodecode@users.noreply.github.com>
2025-03-01 20:47:36 -0300

    Update s7-flatpack.sh

--- s7-flatpack.sh
+++ s7-flatpack.sh
@@ -2,7 +2,7 @@
 set -e
 target="Ubuntu 24.04"
 stage="Stage 7"
-echo "$stage - $target Install flatpack"
+echo "$stage - $target Install flatpak"
 display_header() {
     echo "=================================================="
     echo -e "\n\033[1;34m($stage)>> $1\033[0m"
@@ -14,12 +14,15 @@ display_header "Checking sudo access"
 sudo -v
 
 # ============================================================
-display_header "Installing Visual Studio Code"
+display_header "Installing tor"
 # Install dependencies
-if ! command_exists code; then
-    echo "vscode already installed"
+if ! command_exists flatpak; then
+    echo "installing"
+    
+flatpak install flathub org.torproject.torbrowser-launcher
+    
 else
-    echo "vscode already installed"
+    echo "already installed"
 fi
 
 # ============================================================
